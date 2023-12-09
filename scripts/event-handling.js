7687fcda4676ca376a77c32edf159c67ab091441
austinmpask <austinmpask@gmail.com>
2023-12-08 21:58:14 -0800

    init

--- scripts/event-handling.js
+++ scripts/event-handling.js
@@ -1,21 +1,23 @@
-window.addEventListener('DOMContentLoaded', () => {
-  console.log('event-handling.js script was successfully loaded');
-  const cogIcon = document.querySelector('.fas.fa-cog');
-  cogIcon.addEventListener('click', event => {
-    console.log('Gear clicked');
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".pref").classList.add("pref--hidden");
+  document.querySelector(".search-modal").classList.add("search-modal--hidden");
+  console.log("event-handling.js script was successfully loaded");
+  const cogIcon = document.querySelector(".fas.fa-cog");
+  cogIcon.addEventListener("click", (event) => {
+    console.log("Gear clicked");
     event.stopPropagation();
-    document.querySelector('.pref').classList.remove('pref--hidden');
+    document.querySelector(".pref").classList.remove("pref--hidden");
   });
 
-  window.addEventListener('click', () => {
-    console.log('HTML document clicked');
-    document.querySelector('.pref').classList.add('pref--hidden');
+  window.addEventListener("click", () => {
+    console.log("HTML document clicked");
+    document.querySelector(".pref").classList.add("pref--hidden");
   });
 
-  const searchIcon = document.querySelector('.fa.fa-search');
-  searchIcon.addEventListener('click', () => {
-    console.log('Search clicked');
-    const modal = document.querySelector('.search-modal');
-    modal.classList.toggle('search-modal--hidden');
+  const searchIcon = document.querySelector(".fa.fa-search");
+  searchIcon.addEventListener("click", () => {
+    console.log("Search clicked");
+    const modal = document.querySelector(".search-modal");
+    modal.classList.toggle("search-modal--hidden");
   });
 });
